be2e6fd139a7af68abea9c5ecc77a09a3f42ba40
Olle Harstedt <olleharstedt@yahoo.com>
2020-10-13 18:31:52 +0200

    Add comments

--- index.php
+++ index.php
@@ -6,15 +6,17 @@ use App\Button;
 use App\ButtonGroup;
 use App\Renderer;
 
-$renderer = new Renderer();
-$renderer->filetype = 'php';
-
+// Default viewfile for a class is classname, but can be changed by setting the $viewfile property
 $button1 = new Button(['label' => 'Button 1']);
 $button2 = new Button(['label' => 'Button 2']);
 $buttonGroup = new ButtonGroup(['buttons' => [$button1, $button2]]);
 
+// Default view folder is views/
+$renderer = new Renderer();
+// Check for viewfiles that end with .php
+$renderer->filetype = 'php';
 echo $renderer->render($buttonGroup);
 
+// Switch to .json viewfiles
 $renderer->filetype = 'json';
-
 echo $renderer->render($buttonGroup);
